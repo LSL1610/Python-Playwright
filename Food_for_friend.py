5287f4d6461ca7a1072f1198077f6e7e7cac8899
Le Son Lam <166104490+LSL1610@users.noreply.github.com>
2024-04-11 14:47:38 +0700

    DonE

--- Food_for_friend.py
+++ Food_for_friend.py
@@ -4,7 +4,7 @@ from time import time, sleep
 from loguru import logger
 
 host_name = 'https://shopeefood.vn/'
-product_name = 'Trà sữa'
+product_name = 'su kem'
 
 @logger.catch()
 def get_some_food_info(host_name, product_name):
@@ -19,18 +19,25 @@ def get_some_food_info(host_name, product_name):
         context.set_default_navigation_timeout(60000)
         
         page = context.new_page()
-        try:
-            page.goto(host_name)
-            page.bring_to_front()
-            page.get_by_placeholder('Tìm địa điểm, món ăn, địa chỉ...').fill(product_name)
-            page.locator("a > .btn").click()
-            sleep(5)
-            list_name = page.locator('//*[@id="app"]/div/div[1]/div[2]/div/div/a/div/div/h4').all()
-            logger.debug(type(list_name))
-            for item in list_name:
-                page.get_attribute(item, 'title')
-            page.close()
-        except Exception as e:
-            logger.debug(e)
+        with open('bubletea.txt', 'w', encoding='utf-8') as f:
+            try:
+                page.goto(host_name)
+                page.bring_to_front()
+                page.get_by_placeholder('Tìm địa điểm, món ăn, địa chỉ...').fill(product_name)
+                page.locator("a > .btn").click()
+                sleep(5)
+                number_of_list = page.query_selector_all('//*[@id="app"]/div/div[1]/ul/li')
+                logger.debug(f'number of page product is ==> {len(number_of_list) - 2}')
+                for i in range(2, len(number_of_list) - 2):
+                    page.locator(f'//*[@id="app"]/div/div[1]/ul/li[{i}]').click()
+                    sleep(3)
+                    list_name = page.query_selector_all('//*[@id="app"]/div/div[1]/div[2]/div/div/a/div/div/h4')
+                    for item in list_name:
+                        brand = item.get_attribute('title')
+                        logger.debug(brand)
+                        f.write(brand + '\n')
+                page.close()
+            except Exception as e:
+                logger.debug(e)
         
 get_some_food_info(host_name, product_name)
